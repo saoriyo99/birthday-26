42b0631ca9bd7614e783fbd1ab1ea887f53a2fda
saoriyo99 <saoriyo99@yahoo.com>
2025-08-05 13:41:32 -0400

    build(webpack): Set publicPath to 'auto' for flexible deployments
    Change the hardcoded `publicPath` from `'/birthday-26/'` to `'auto'`.
    
    This allows Webpack to automatically determine the correct asset path at runtime. It makes the application more portable and easier to deploy to different environments (like GitHub Pages) without being tied to a specific subdirectory.

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
-    publicPath: '/birthday-26/',
+    publicPath: 'auto',
     clean: true,
   },
   module: {
